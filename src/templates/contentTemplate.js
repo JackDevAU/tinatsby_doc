c83ecfbd10fd209fd8982c7152cb2c30e2d0f3c1
JackDevAU <jpr178@live.com.au>
2024-10-11 10:31:46 +1000

    Refactor contentTemplate.js to conditionally render editable fields

--- src/templates/contentTemplate.js
+++ src/templates/contentTemplate.js
@@ -1,9 +1,11 @@
-"use client";
+
 import React, { useRef } from "react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
-import { useTina, tinaField } from "tinacms/dist/react";
+import { useTina, tinaField, useEditState } from "tinacms/dist/react";
 
 const ContentTemplate = ({ pageContext }) => {
+  const { edit } = useEditState()
+
   const { query, variables, parsedMdx } = pageContext;
 
   const { data } = useTina({
@@ -11,19 +13,24 @@ const ContentTemplate = ({ pageContext }) => {
     variables: variables,
     data: parsedMdx,
   });
-  console.log(data);
-
-  // const refData = useRef(data);
-
+  
+  if(edit){
+    return (
+      <div>
+        <h1 data-tina-field={tinaField(data?.post, "title")}>
+          {data?.post?.title}
+        </h1>
+  
+        <div data-tina-field={tinaField(data?.post, "body")}>
+          <TinaMarkdown content={data?.post?.body} />
+        </div>
+      </div>
+    );
+  }
+  
   return (
     <div>
-      <h1 data-tina-field={tinaField(data?.post, "title")}>
-        {data?.post?.title}
-      </h1>
-
-      <div data-tina-field={tinaField(data?.post, "body")}>
-        <TinaMarkdown content={data?.post?.body} />
-      </div>
+      <TinaMarkdown content={data} />
     </div>
   );
 };
